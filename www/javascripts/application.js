afd9b5421a8733453f5d34b66cf74bda105f2604
Ben <bdejesus@engineyard.com>
2010-04-02 11:27:22 -0700

    added script to add click tracking to download links for analytics

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+  $('.trackDownloads a').click(function(){
+    var filename = this.pathname.substr(this.pathname.lastIndexOf("/")+1,this.pathname.length);
+    _gaq.push(['_trackEvent', 'download', filename]);
+    return true;
+  });
+});
