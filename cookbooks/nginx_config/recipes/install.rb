9b3618183ba91596f2b1532a0d17718952be1391
Nick Sieger <nick@nicksieger.com>
2012-01-12 08:20:37 -0600

    Add cron to get new javadocs every night and change javadoc location

--- cookbooks/nginx_config/recipes/install.rb
+++ cookbooks/nginx_config/recipes/install.rb
@@ -21,3 +21,13 @@ remote_file "/data/nginx/servers/jruby.conf" do
   mode 0644
   notifies :restart, resources(:service => "nginx"), :delayed
 end
+
+cron "apidocs" do
+  minute    '10'
+  hour      '2'
+  day       '*'
+  month     '*'
+  weekday   '*'
+  command   'cd /data && wget --mirror http://ci.jruby.org/job/jruby-dist-release/javadoc/ > /dev/null'
+  action :create
+end
