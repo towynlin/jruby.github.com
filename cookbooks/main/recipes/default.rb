09eeb9451ea40958d4ad5a14a10e074763790845
Nick Sieger <nick@nicksieger.com>
2012-05-14 16:02:23 -0500

    Preserve ssh host keys across reboots

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,4 +1,17 @@
 
+service "sshd" do
+  action :nothing
+  supports :restart => true
+end
+
+Dir['/data/etc/ssh/*'].each do |sshf|
+  execute "preserve ssh host keys" do
+    command "cp -p #{sshf} /etc/ssh"
+    notifies :restart, resources(:service => "sshd"), :delayed
+    not_if { system "cmp #{sshf} /etc/ssh/#{File.basename(sshf)} > /dev/null"; $?.success? }
+  end
+end
+
 require_recipe 'nginx_config'
 
 require_recipe 'git-user'
